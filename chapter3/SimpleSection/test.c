0566d8847633d55a41d8e3e22379dcc8c6514769
jackyztli <jacky.szu.edu.cn@gmail.com>
2019-09-13 08:26:37 +0000

    elf construct

---
+++ chapter3/SimpleSection/test.c
@@ -0,0 +1,9 @@
+// File Name: test.c
+// Author: jacky
+// Created Time: Fri 13 Sep 2019 05:32:51 AM UTC
+static int x1 = 0;
+static int x2 = 1;
+static int* p = 0;
+static char* str = "\0";
+
+__attribute__((section("JACKY"))) int tmp = 42;
